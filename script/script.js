d5baec8bac16b902a57af29be42941a793d7fa48
solomon <solomonsitotaw89@gmail.com>
2023-09-15 12:48:25 +0300

    change the DOM manipulation with JQuery

--- script/script.js
+++ script/script.js
@@ -1,111 +1,26 @@
-
-{
-    function displayList1(e) {
+function displayList1(e) {
     e.preventDefault();
-    var x = document.querySelectorAll("#shop");
-    for (i = 0; i < x.length;i++){
-        x[i].style.display="block";
-    }
-    
-}
-function hideList1() {
-    var b = document.querySelectorAll("#shop");
-    for (i = 0; i < b.length;i++){
-        b[i].style.display="none";
-    }
-    
+    $(".shop li").toggle();  
 }
 function displayList2(e) {
     e.preventDefault();
-    var x = document.querySelectorAll("#Services");
-    for (i = 0; i < x.length;i++){
-        x[i].style.display="block";
-    }  
-}
-function hideList2() {
-    var b = document.querySelectorAll("#Services");
-    for (i = 0; i < b.length;i++){
-        b[i].style.display="none";
-    }
-    
+    $(".Services li").toggle(); 
 }
 function displayList3(e) {
     e.preventDefault();
-    var x = document.querySelectorAll("#Apple_Store");
-    for (i = 0; i < x.length;i++){
-        x[i].style.display="block";
-    }
-    
-}
-function hideList3() {
-    var b = document.querySelectorAll("#Apple_Store");
-    for (i = 0; i < b.length;i++){
-        b[i].style.display="none";
-    }
-    
+    $(".Apple_Store li").toggle();   
 }
 function displayList4(e) {
     e.preventDefault();
-    var x = document.querySelectorAll("#val");
-    for (i = 0; i < x.length;i++){
-        x[i].style.display="block";
-    }
-    
-}
-function hideList4() {
-    var b = document.querySelectorAll("#val");
-    for (i = 0; i < b.length;i++){
-        b[i].style.display="none";
-    }
-    
-    }
-}
-
-{
-    {
-        var y = document.getElementById("sl");
-    y.addEventListener("click", displayList1);
-
-var a = document.getElementById("sl");
-    a.addEventListener("dblclick", hideList1);}
-
-{var y = document.getElementById("ser");
-    y.addEventListener("click", displayList2);
-    
- var a = document.getElementById("ser");
-    a.addEventListener("dblclick", hideList2); }  
-        
-{var y = document.getElementById("as");
-    y.addEventListener("click", displayList3);
-
-var a = document.getElementById("as");
-    a.addEventListener("dblclick", hideList3);}
-
-{var y = document.getElementById("av");
-    y.addEventListener("click", displayList4);
-
-var a = document.getElementById("av");
-    a.addEventListener("dblclick", hideList4);}
-
+    $(".val li").toggle();  
 }
 function dis(e) {
     e.preventDefault();
-    var x = document.querySelectorAll(".navlink");
-    console.log(x[0])
-    for (i = 0; i < x.length;i++){
-        x[i].style.display = "block";
-        x.style.transition="2s";
-    }
+    $(".navlink").slideToggle();
+   
 }
-function ndis(f) {
-    f.preventDefault();
-    var x = document.querySelectorAll(".navlink");
-    console.log(x[0])
-    for (i = 0; i < x.length;i++){
-        x[i].style.display = "none";
-    }
-}
-var y = document.getElementById("yes");
-y.addEventListener("click", dis)
-var k = document.getElementsByClassName("xmark");
-k.addEventListener("click", ndis);
+$("#sl").on("click", displayList1);
+$("#ser").on("click", displayList2);
+$("#as").on("click", displayList3);
+$("#av").on("click", displayList4);
+$("#yes").on("click", dis)
